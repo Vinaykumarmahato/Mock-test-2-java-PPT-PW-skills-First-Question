90b1a697353bf636b531ca5d040bb33f9f13f3c0
ADV Indian coder <116486037+ADVindiancoder@users.noreply.github.com>
2023-07-12 08:56:22 -0700

    Mock_test_2_java_PPT_PW_skills_First_Questio

---
+++ Mock-test-2-java-PPT-PW-skills-Question Public/Mock_test_2_java_PPT_PW_skills_First_Question_Public.java
@@ -0,0 +1,42 @@
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
+
+public class Mock_test_2_java_PPT_PW_skills_First_Question_Public {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("John");
+        person.setAge(30);
+        person.setAddress("123 Main St");
+
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Address: " + person.getAddress());
+    }
+}
